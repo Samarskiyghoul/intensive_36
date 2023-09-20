b4c38b08f7a51f35c8fac62f6e6c3b985176c2d6
Anton Teslia <teslyaanton@yandex.ru>
2023-09-20 12:33:35 +0300

    feat: add class IntensiveArrayList_AntonTeslia

---
+++ intensive_36/src/task_1_1/impl/IntensiveArrayList_AntonTeslia.java
@@ -0,0 +1,40 @@
+package task_1_1.impl;
+
+import task_1_1.IntensiveList;
+
+public class IntensiveArrayList_AntonTeslia implements IntensiveList {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean add(Object o) {
+        return false;
+    }
+
+    @Override
+    public void add(int index, Object element) {
+
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public Object set(int index, Object element) {
+        return null;
+    }
+
+    @Override
+    public Object remove(int index) {
+        return null;
+    }
+
+    @Override
+    public void removeAll() {
+
+    }
+}
