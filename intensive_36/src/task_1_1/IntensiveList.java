00233884ff95cde4b5b74fe8616e2239820161b3
Vera Cherkasova <1111@mail.com>
2023-09-26 21:39:01 +0300

    Homework1

--- intensive_36/src/task_1_1/IntensiveList.java
+++ intensive_36/src/task_1_1/IntensiveList.java
@@ -1,18 +1,17 @@
-package task_1_1;
+package task_1_1.impl.task_1_1;
 
-public interface IntensiveList<E> {
+import java.util.Comparator;
 
+public interface IntensiveList<T> {
+
+    T getItem(int index);
     int size();
+    void addItem(T item);
+    void update(int index, T item);
+    void delete(int index);
+    void increaseArray();
+    void printArray();
+    void sort(Comparator<? super T> c);
+    void filterNulls();
 
-    boolean add(Object o);
-
-    void add(int index, Object element);
-
-    Object get(int index);
-
-    Object set(int index, Object element);
-
-    Object remove(int index);
-
-    void removeAll();
 }
